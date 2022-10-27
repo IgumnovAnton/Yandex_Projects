c26f449c71c4d3d358edf5e3fbbe66d39d6faa4d
IgumnovAnton <vaigumnov@gmail.com>
2022-10-27 19:39:27 +0400

    git_ignore and db_config added

---
+++ Telecom_Project/db_config.py
@@ -0,0 +1,7 @@
+db_config = {
+'user': 'praktikum_student', # имя пользователя,
+'pwd': 'Sdf4$2;d-d30pp', # пароль,
+'host': 'rc1b-wcoijxj3yxfsf3fs.mdb.yandexcloud.net',
+'port': 6432, # порт подключения,
+'db': 'data-science-final' # название базы данных,
+} 
