2e1b0cf588aaf2f545de16065f424198ea21f4af
IgumnovAnton <vaigumnov@gmail.com>
2022-10-27 20:18:16 +0400

    ignore updated

--- Telecom_Project/db_config.py
+++
@@ -1,7 +0,0 @@
-db_config = {
-'user': 'praktikum_student', # имя пользователя,
-'pwd': 'Sdf4$2;d-d30pp', # пароль,
-'host': 'rc1b-wcoijxj3yxfsf3fs.mdb.yandexcloud.net',
-'port': 6432, # порт подключения,
-'db': 'data-science-final' # название базы данных,
-} 
